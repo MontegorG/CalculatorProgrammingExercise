ea299eddcf26bf3233b9afe456dc0a1ca4cccb77
MontegorG <55543094+MontegorG@users.noreply.github.com>
2022-06-27 10:06:52 -0400

    Initial Commit
    Has all the initial code for the calculator app.

---
+++ Model/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammingExercise.Model
+{
+    public class Calculator
+    {
+        [Required]
+        public double Num1 { get; set; }
+
+        [Required]
+        public Pages.Index.operation op { get; set; }
+
+        [Required]
+        public double Num2 { get; set; }
+    }
+}
